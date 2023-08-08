4fb18124e4ce5581b59d0cc053498e34c5ad1792
Андрей Репринцев <Randrew8313@gmail.com>
2023-08-08 23:01:44 +0300

    Задача 8

---
+++ Task008_AllEvenNum/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+int num = new Random().Next(1, 10);
+Console.Write("Исходное Число ");
+Console.WriteLine(num);
+
+int count = 1;
+
+while(count <= num)
+{
+    if(count % 2 == 0)
+    {
+        Console.WriteLine(count);
+    }
+    count++;
+}
